5ddcc2dae3d2cc385b6f961d67988ead97fadad0
Demki <mordbz@gmail.com>
2019-08-16 16:01:01 +0300

    changed seed generation to base64

--- dex/prng.js
+++ dex/prng.js
@@ -32,14 +32,11 @@ function sfc32(a, b, c, d) {
 
 ///// And this one https://stackoverflow.com/a/27747377/8491228 /////
 
-function dec2hex (dec) {
-  return ('0' + dec.toString(16)).substr(-2)
-}
 
 function generateId (len) {
-  var arr = new Uint8Array((len || 40) / 2)
-  window.crypto.getRandomValues(arr)
-  return Array.from(arr, dec2hex).join('')
+  var arr = new Uint8Array(Math.ceil((len || 40) * 3 / 4));
+  window.crypto.getRandomValues(arr);
+  return base64js.fromByteArray(arr).substring(0,len);
 }
 
 function shuffle(rand, a) {
